216f705f456945f4b707fbc07daa6f9879c84a7b
Daniel Nettle <daniel.nettle@ncl.ac.uk>
2021-01-13 10:36:44 +0000

    minor edits

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ from os import environ
 from fractions import Fraction
 SESSION_CONFIG_DEFAULTS = dict(real_world_currency_per_point=3, participation_fee=0)
 SESSION_CONFIGS = [dict(name='experiment', num_demo_participants=2,
-	initial_mean_energy_level=105, initial_std_energy_level = 5,number_mock_rounds=3,r=0.1,mean_regression=False, 
+	initial_mean_energy_level=105, initial_std_energy_level = 5,number_mock_rounds=3,r=0.1,mean_regression=True, 
 	steal_success_prob = Fraction(1,3),victims_payoff=10,energy_stochasticity=3,energy_cost=0,
 	cooperation_payoff=5,penalty=50,threshold=100,lone_payoff=0,steal_failure_payoff=-40,steal_success_payoff=20, 
 	app_sequence=['instructions','mock_rounds','end_of_mock_rounds', 'real_rounds', 'debrief'])]
